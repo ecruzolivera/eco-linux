b7ce191ee7a0d67ce74a0c41fea8f8c2b6f3b31e
Ernesto Cruz Olivera <ecruzolivera@gmail.com>
2026-05-20 00:18:22 -0600

    just ping google dns

--- lib/00-checks.sh
+++ lib/00-checks.sh
@@ -25,7 +25,7 @@ do_checks() {
   info "Target user: $REAL_USER"
   info "Target home: $REAL_HOME"
 
-  if ! ping -c 1 -W 2 archlinux.org &>/dev/null && ! ping -c 1 -W 2 google.com &>/dev/null; then
+  if ! ping -c 1 -W 1 8.8.8.8 >/dev/null 2>&1; then
     error "No internet connection"
   fi
   info "Internet OK"
