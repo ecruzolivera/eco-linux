f5d351bb36b2051ba3842633630420ea588a3869
Ernesto Cruz Olivera <ecruzolivera@gmail.com>
2026-05-20 17:50:46 -0600

    fixing chezmoi

--- scripts/08-chezmoi.sh
+++ scripts/08-chezmoi.sh
@@ -6,10 +6,10 @@ chown -R "$REAL_USER":"$REAL_USER" "$USER_CONFIG" 2>/dev/null || true
 
 if [ -d "$CHEZMOI_SOURCE_DIR" ]; then
   info "chezmoi already initialized, running update..."
-  sudo -u "$REAL_USER" chezmoi update
+  sudo -Hu "$REAL_USER" chezmoi update
 else
   info "Initializing chezmoi from dotfiles repo..."
-  sudo -u "$REAL_USER" chezmoi init --apply https://github.com/ecruzolivera/dotfiles.git
+  sudo -Hu "$REAL_USER" chezmoi init --apply https://github.com/ecruzolivera/dotfiles.git
 fi
 
 info "chezmoi bootstrap complete"
