22428f34c7feff7123d3735cc000589a203af456
Ernesto Cruz Olivera <ecruzolivera@gmail.com>
2026-05-20 02:33:27 -0600

    fix: glob copy issue

--- scripts/copy-config.sh
+++ scripts/copy-config.sh
@@ -2,6 +2,6 @@ phase "copy xdg-config-home"
 
 mkdir -p "$USER_CONFIG"
 
-cp -rv "$INSTALLER_XDG_CONFIG_DIR/*" "$USER_CONFIG/"
+cp -rv "$INSTALLER_XDG_CONFIG_DIR"/. "$USER_CONFIG/"
 
 info "copy config complete"
